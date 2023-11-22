debf017bcaccd128758b3a6bd764aa83654199c4
Laksa Bayu <laksabayu18.com>
2023-11-23 01:44:01 +0700

    first commit httprouter

---
+++ serve_file_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServerFile(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:4000/files/hello.txt",nil) //nil itu body
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder,request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello LaksaRouter", string(body))
+}
